5122a6cffbe52901dfe65db70c569fb06c81e674
Chiara Passaro <chiarapassaro.wd@gmail.com>
2020-02-27 10:54:19 +0100

    select

--- index.php
+++ index.php
@@ -1,31 +1,47 @@
-<!DOCTYPE html>
-<html lang="en">
+ <?php
+  include 'partials/header.php';
+  ?>
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
+ <body>
+   <header>
+     <div class="container">
+       <img class="logo" src="http://pluspng.com/img-png/spotify-logo-png-open-2000.png" alt="">
+       <?php // include 'database.php' 
+        ?>
+       <select name="search" id="search-cd">
+         <option value="">All</option>
+         <?php //foreach ($database as $value) { 
+          ?>
+         <!-- <option value="<?php //echo $value['author'] 
+                              ?>"><?php // echo $value['author'] 
+                                  ?></option> -->
+         <?php //} 
+          ?>
+       </select>
+     </div>
+   </header>
+   <!-- container dei cd -->
+   <div class="cds">
 
-<body>
-  
-  <!-- container dei cd -->
-  <div class="cds">
+   </div>
 
-  </div>
-
-  <!-- template cd -->
-  <script id="entry-template" type="text/x-handlebars-template">
-    <div class="cd">
-      <img class="cd-image" src="" alt="">
-      <h2 class="cd-title"></h2>
-      <h3 class="cd-author"></h3>
-      <small class="cd-date"></small>
+   <!-- template cd -->
+   <script id="entry-template" type="text/x-handlebars-template">
+     <div class="cd">
+      <img class="cd-image" src="{{poster}}" alt="">
+      <h2 class="cd-title">{{title}}</h2>
+      <h3 class="cd-author">{{author}}</h3>
+      <small class="cd-date">{{year}}</small>
     </div>
   </script>
 
-  <!-- nostro script -->
-  <script src="dist/app.js"></script>
-</body>
+   <!-- template cd -->
+   <script id="entry-option" type="text/x-handlebars-template">
+     <option value="{{author}}">{{author}}</option>
+   </script>
 
-</html>
+   <!-- nostro script -->
+   <script src="dist/app.js"></script>
+ </body>
+
+ </html>
--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,68 @@ const Handlebars = require("handlebars");
 
 $(document).ready(function(){
   //alert('Ciao');
+  $.ajax({
+    url: 'http://localhost:8888/php-ajax-dischi/server.php',
+    method: 'GET',
+    success: function(data){
+      console.log(data);
+      if(data.length > 0) {
+        printData(data);
+      } else {
+        alert('Nessun risultato');
+      }
+    },
+    error: function(){
+      alert('Errore');
+    }
+  });
+
+  printSelect();
 
 });
+
+function printData(data){
+  var source = $('#entry-template').html();
+  var template = Handlebars.compile(source);
+
+  for (var i = 0; i < data.length; i++) {
+    var cd = data[i];
+    var html = template(cd);
+    $('.cds').append(html);
+  }
+}
+
+function printSelect() {
+
+  $.ajax({
+    url: 'http://localhost:8888/php-ajax-dischi/server.php',
+    method: 'GET',
+    data: {
+      'author-list': true
+    },
+    success: function (data) {
+      console.log(data);
+      if (data.length > 0) {
+        var source = $('#entry-option').html();
+        var template = Handlebars.compile(source);
+        for(var i = 0; i< data.length; i++ ) {
+          var context = {
+            'author': data[i]
+          };
+          var html = template(context);
+          $('#search-cd').append(html);
+        }
+        
+      } else {
+        alert('Nessun risultato');
+      }
+    },
+    error: function () {
+      alert('Errore');
+    }
+  });
+
+
+
+  
+}
